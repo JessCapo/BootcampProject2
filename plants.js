77c33a40836212e4bf16dd28e85fba3f7e8c83e7
JessCapo <jlcapob@gmail.com>
2025-09-06 15:36:04 -0400

    added style sheet

--- plants.js
+++ plants.js
@@ -1,9 +1,22 @@
+const categoryContainer = document.querySelector('.js-category-container')
+
 function loadCategories() {
-    fetch("https://plants10.p.rapidapi.com/plants/details")
+    fetch('https://my-api.plantnet.org/v2/identify/all?api-key=2b10jjhQC8Otf6gYnhXG0s3Ve')
     .then(data => data.json())
     .then(response => {
         const categories = response.categories;
-        console.log(categories);
+        
+
+        categoriesContainer.innerHTML = categories
+            .map(category => `
+                <div class="category-item" data-id="${category.idCategory}">
+                    <h2>${category.strCategory}</h2>
+                    <img 
+                        src="${category.strCategoryThumb}" 
+                        alt="${category.strCategory}" />
+                    <p>${category.strCategoryDescription}</p>
+                </div>
+            `)
 
 
 
