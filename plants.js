f88d564a88438146a5478a83399dfb1a729996af
JessCapo <jlcapob@gmail.com>
2025-09-07 11:42:01 -0400

    changes to html page and search bar

--- plants.js
+++ plants.js
@@ -1,9 +1,21 @@
 $(document).ready(()=>{
+    $("#submit").click(()=>{
+        let userInput = $("#search").val()
+        //alert(userInput)
+        $.ajax({
+        url:`https://perenual.com/api/species-care-guide-list?key=sk-OhIh68bd8913791b012262=${userInput}`
+    })
+        .done((res)=>{
+            let plants = res.Search;
+            $.each(plants, (i, e)=>{
+                let poster = e.Poster
+                console.log("poster", poster)
+                $("body").append(`<img src="${poster}" alt="plant image"/>`)
+            })
+            
 
-    $.ajax({
-        url:https:'//perenual.com/api/v2/species-list?key=sk-OhIh68bd8913791b012262'
 
-
+        })
     })
 
 
